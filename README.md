542802a5c7f137752f1e52983399cc6c4f0a0b51
Filip Kin <me@filipkin.com>
2024-01-01 01:02:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# WAYDRN
+
+What Are You Doing Right Now
++
+How Are You Feeling Right Now
+
+## Setup
+
+### Get API Credentials
+
+You will need to [enable the api](https://console.cloud.google.com/flows/enableapi?apiid=sheets.googleapis.com)
+then [create credentials](https://developers.google.com/sheets/api/quickstart/nodejs#authorize_credentials_for_a_desktop_application)
+
+Copy paste your client email and private key into the app.js on line 23 & 24
+
+### Getting the spreadsheet
+
+Make a copy of this [template](https://docs.google.com/spreadsheets/d/13kDO33IB1nm56qlHeq9o9EmHaJsOyPZ9Of2C10O83-E/edit?usp=sharing)
+Share it with your email, make sure it has editing rights
+
+### The client-side
+
+Currently client side is handled with the app [Tasker](https://play.google.com/store/apps/details?id=net.dinglisch.android.taskerm&hl=en_US&gl=US&pli=1) So you'll want to install that
+Sorry Apple users
+
+The tasks and profile are in the repository, you'll just have to replace the ip address in them
---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Set your port if it's different
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const doc = new GoogleSpreadsheet('1Wy-54vI-l8dD-EZ_LTBgKdRT3DOh3YflAPzu4e9kT9Y'); // Document ID here
+
+function getDay(dateParam, timeParam) {
+    let date = new Date(dateParam);
+    let time = parseInt(timeParam.split('.')[0])
+
+    let start = new Date(date.getFullYear(), 0, 0);
+    let diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
+    let oneDay = 1000 * 60 * 60 * 24;
+    let day = Math.floor(diff / oneDay);
+
+    return [date, day, time]
+}
+
+(async () => {
+
+    // Your google API credentials here
+    await doc.useServiceAccountAuth({
+        client_email: "",
+        private_key: "",
+    });
+    await doc.loadInfo();
+    const waydrnSheet = doc.sheetsByIndex[0];
+    const hayfrnSheet = doc.sheetsByIndex[1];
+
+    app.get('/waydrn/:date/:time/:selection', async (req, res) => {
+        let info = getDay(req.params.date, req.params.time);
+        let date = info[0];
+        let day = info[1];
+        let time = info[2];
+
+        console.log(`[${day} ${time}] 
+WAYDRN ${req.params.selection - 1}`);
+        res.send('Recorded');
+        await waydrnSheet.loadCells({ startRowIndex: day, startColumnIndex: time + 2 });
+        waydrnSheet.getCell(day, time + 2).value = req.params.selection - 1;
+
+
+
+        await waydrnSheet.saveUpdatedCells();
+    });
+
+    app.get('/hayfrn/:date/:time/:selection', async (req, res) => {
+        let info = getDay(req.params.date, req.params.time);
+        let date = info[0];
+        let day = info[1];
+        let time = info[2];
+
+        console.log(`[${day} ${time}] HAYFRN ${req.params.selection - 1}`);
+        res.send('Recorded');
+        await hayfrnSheet.loadCells({ startRowIndex: day, startColumnIndex: time + 2 });
+        hayfrnSheet.getCell(day, time + 2).value = req.params.selection - 1;
+
+
+
+        await hayfrnSheet.saveUpdatedCells();
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+})()
